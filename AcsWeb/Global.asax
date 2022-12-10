4c51eb155ca02c29457f9cd39ef6b3ea9ff28e6b
lavanya <kanneboinal17@gmail.com>
2022-12-10 11:39:03 +0530

    Initial project commit

---
+++ AcsWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AcsWeb.MvcApplication" Language="C#" %>
---
+++ AcsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcsWeb.Startup))]
+namespace AcsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
